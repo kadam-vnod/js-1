c44c3f38c864ae5f7a080257afeb46af20bf151a
kadam-vnod <147443911+kadam-vnod@users.noreply.github.com>
2023-10-13 07:23:10 +0000

    config done

---
+++ 01_basicss/01_variables.js
@@ -0,0 +1,10 @@
+
+let id = "123abc"
+const phno = 9010203207;
+var name  = "vnod";
+
+console.log(
+    `id=${id},
+    phno=${phno},
+    name=${name}`
+    );
---
+++ 01_basicss/02_datatypes.js
@@ -0,0 +1,8 @@
+String ="kadam"
+Number = 1234
+Boolean = true // or false
+undefined =
+//null = null
+
+
+console.log(`${String},${Number},${Boolean},${undefined},${null}`);
