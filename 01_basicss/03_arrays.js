66322200d1f86f72c65d47499f23f7a75abf47ba
kadam-vnod <147443911+kadam-vnod@users.noreply.github.com>
2023-10-13 07:55:39 +0000

    config done

---
+++ 01_basicss/03_arrays.js
@@ -0,0 +1,15 @@
+const cars = ["bmw", "audi" ,"honda"]
+
+cars.push("jag","hii")
+
+//console.log(cars);
+
+
+const fruits =[]
+fruits.push("mango","apple","jamkaya")
+//console.log(fruits);
+console.log(Object.keys(fruits));
+console.log(Object.values(fruits));
+
+
+
